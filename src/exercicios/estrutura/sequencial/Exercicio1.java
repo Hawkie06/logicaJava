81e7deb407ebe843a98b8f67c9faddaba4405be5
Náthally <gaviaonathally2006@gmail.com>
2026-08-06 16:00:33 -0300

    Exercicio soma

--- src/exercicios/estrutura/sequencial/Exercicio1.java
+++ src/exercicios/estrutura/sequencial/Exercicio1.java
@@ -1,5 +1,15 @@
 package exercicios.estrutura.sequencial;
 
 public class Exercicio1 {
+    public static void main(String[] args) {
+        int num1 = 30;
+        int num2 = 50;
+        int soma = num1 + num2;
 
+        System.out.println("A soma do número " + num1 + " + " + num2 + " é " + soma);
+
+    }
 }
+
+/*Faça um programa para ler dois valores inteiros, e depois mostrar na tela a soma desses números com uma*
+ *mensagem explicativa.*/
